32e3afea76e3a9d181d8926a28a54dcbb7a82bcc
Aimable <raimable@gmail.com>
2018-11-12 15:04:34 +0200

    Allowing null for nationality in Payload since some playloads do not have a nationality

--- db/migrate/20181112085807_create_payloads.rb
+++ db/migrate/20181112085807_create_payloads.rb
@@ -2,7 +2,7 @@ class CreatePayloads < ActiveRecord::Migration[5.1]
   def change
     create_table :payloads do |t|
       t.references :mission, foreign_key: true
-      t.references :nationality, foreign_key: true
+      t.references :nationality, foreign_key: true,null:true
       t.string :payload_identifier
       t.boolean :reused
       t.string :manifacturer
