9838846fbb33b489b2fecd7946b3a9bca7518661
Aimable <raimable@gmail.com>
2018-11-12 16:14:39 +0200

    Typo fix. Renamed manifacturer to manufacturer

--- db/migrate/20181112085807_create_payloads.rb
+++ db/migrate/20181112085807_create_payloads.rb
@@ -2,10 +2,10 @@ class CreatePayloads < ActiveRecord::Migration[5.1]
   def change
     create_table :payloads do |t|
       t.references :mission, foreign_key: true
-      t.references :nationality, foreign_key: true,null:true
+      t.references :nationality, foreign_key: true, null:true
       t.string :payload_identifier
       t.boolean :reused
-      t.string :manifacturer
+      t.string :manufacturer
       t.string :payload_type
       t.float :mass_kg
       t.float :mass_lbs
