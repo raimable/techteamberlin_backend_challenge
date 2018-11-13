5965174f93b8005273dd0b9694e6103d84d16285
Aimable <raimable@gmail.com>
2018-11-13 20:40:04 +0200

    Corrected code indentation

--- app/controllers/payloads_controller.rb
+++ app/controllers/payloads_controller.rb
@@ -1,12 +1,12 @@
 class PayloadsController < ApplicationController
 
-	def index
-		@payloads = Payload.all
-		@payloads = @payloads.where(nationality_id: params[:nationality_id]) if params[:nationality_id]
+  def index
+    @payloads = Payload.all
+    @payloads = @payloads.where(nationality_id: params[:nationality_id]) if params[:nationality_id]
 
-	end
+  end
 
-	def show 
-		@payload= Payload.find(params[:id])
-	end  
+  def show
+    @payload= Payload.find(params[:id])
+  end
 end
