32798f5bcdc267e1692f6969e13faf4efeb018f1
Aimable <raimable@gmail.com>
2018-11-13 17:34:43 +0200

    Corrected code indentation

--- app/jobs/fetch_missions_job.rb
+++ app/jobs/fetch_missions_job.rb
@@ -1,60 +1,59 @@
-			require 'net/http'
-			class FetchMissionsJob < ApplicationJob
-			  queue_as :default
+require 'net/http'
+class FetchMissionsJob < ApplicationJob
+  queue_as :default
 
 
-			  retry_on Net::HTTPExceptions
-			  discard_on JSON::JSONError
-			  discard_on ActiveRecord::ActiveRecordError
+  retry_on Net::HTTPExceptions #5 times, 3 seconds apart
+  discard_on JSON::JSONError
+  discard_on ActiveRecord::ActiveRecordError
 
-			  def perform(*args)
-			    # Do something later
-			    sync = args[0] if args
-			    fetch_missions(sync)
-			  end
+  def perform(*args)
+    # Do something later
+    sync = args[0] if args
+    fetch_missions(sync)
+  end
 
-			  def fetch_missions(sync=false)
+  def fetch_missions(sync=false)
 
-			  	#https://docs.ruby-lang.org/en/2.0.0/Net/HTTP.html
+    #https://docs.ruby-lang.org/en/2.0.0/Net/HTTP.html
 
-			    Rails.logger.info "Mission Fetch Job Starts."
+    Rails.logger.info "Mission Fetch Job Starts."
 
-			  	uri = URI(Rails.application.config.x.space_x_api_missions_url)
-			    missions = Net::HTTP.get(uri)
-			    json_data    = JSON.parse(missions) 
-			    
-			    error_count = 0
+    uri = URI(Rails.application.config.x.space_x_api_missions_url)
+    missions = Net::HTTP.get(uri)
+    json_data    = JSON.parse(missions)
 
-			    json_data.each do |mission_data|
+    error_count = 0
 
-			    	#Check if the mission is already there by mission_id to avoid deplicate
-				    mission_identifier = mission_data["mission_id"]
-				    Mission.find_or_create_by mission_identifier:mission_identifier  do |mission|
+    json_data.each do |mission_data|
 
-				    mission.name = mission_data["mission_name"]
-			    	mission.mission_identifier = mission_data["mission_id"]
-			    	mission.wikipedia = mission_data["wikipedia"]
-			    	mission.website = mission_data["website"]
-			    	mission.twitter = mission_data["twitter"]
-			    	mission.description = mission_data["description"]
-					
-					
-				   
+      #Check if the mission is already there by mission_id to avoid deplicate
+      mission_identifier = mission_data["mission_id"]
+      Mission.find_or_create_by mission_identifier:mission_identifier  do |mission|
 
-			        error_count = error_count+1	unless mission.save
+        mission.name = mission_data["mission_name"]
+        mission.mission_identifier = mission_data["mission_id"]
+        mission.wikipedia = mission_data["wikipedia"]
+        mission.website = mission_data["website"]
+        mission.twitter = mission_data["twitter"]
+        mission.description = mission_data["description"]
 
-			        mission_data["payload_ids"].each do |payload_id|
-			        	if sync == true
-			        		FetchPayloadJob.perform_now(payload_id,mission.id)
-			        	else
-			        		FetchPayloadJob.perform_later(payload_id,mission.id)
 
-			        	end
-			        end
-			    end 
 
-			end
-			    Rails.logger.info "Mission Fetch Job Completed. #{error_count} errors out of #{json_data.count}"
-			  end 
+        error_count = error_count+1	unless mission.save
 
-			end
+        mission_data["payload_ids"].each do |payload_id|
+          if sync == true
+            FetchPayloadJob.perform_now(payload_id,mission.id)
+          else
+            FetchPayloadJob.perform_later(payload_id,mission.id)
+
+          end
+        end
+      end
+
+    end
+    Rails.logger.info "Mission Fetch Job Completed. #{error_count} errors out of #{json_data.count}"
+  end
+
+end
