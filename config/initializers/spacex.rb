3c12b86acdaec35adf80bf8ad68e20a721ea78d2
Aimable <raimable@gmail.com>
2018-11-13 17:21:57 +0200

    SpaceX API URLs moved into initializer config

---
+++ config/initializers/spacex.rb
@@ -0,0 +1,2 @@
+Rails.application.config.x.space_x_api_missions_url = 'https://api.spacexdata.com/v3/missions'
+Rails.application.config.x.space_x_api_payloads_url = 'https://api.spacexdata.com/v3/payloads'
