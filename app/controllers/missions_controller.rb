e13fd7aba640da25bbcdfdd950f312b3be85c398
Aimable <raimable@gmail.com>
2018-11-13 20:26:07 +0200

    Updated search function on missions controller

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,21 +1,23 @@
 class MissionsController < ApplicationController
 
-	def index
-		if params[:nationality_id]
-			@missions = Nationality.find(params[:nationality_id]).missions.uniq
-		else
-			@missions = Mission.all
-		end
-	end
+  def index
+    if params[:nationality_id]
+      @missions = Nationality.find(params[:nationality_id]).missions.uniq
+    else
+      @missions = Mission.all
+    end
+  end
 
-	def show 
-		@mission = Mission.find(params[:id])
-	end
+  def show
+    @mission = Mission.find(params[:id])
+  end
 
-	def search
-		params.require(:query)
-		query = params[:query]
-		@missions = Mission.search_by_description(query)
-		render 'index'
-	end
+  def search
+
+    params.require(:query)
+
+    query = params[:query]
+    @missions = Mission.search_by_description(query)
+    render 'index'
+  end
 end
