93b58938b10e1e742b1d9ac1845f81285b4a8884
Aimable <raimable@gmail.com>
2018-11-13 20:41:51 +0200

    Corrected code indentation

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,8 +1,8 @@
 class Mission < ApplicationRecord
-	include PgSearch
-    
-    pg_search_scope :search_by_description, against: :description
+  include PgSearch
 
-	has_many :payloads , dependent: :destroy
-	has_many :nationalities, through: :payloads
+  pg_search_scope :search_by_description, against: :description
+
+  has_many :payloads , dependent: :destroy
+  has_many :nationalities, through: :payloads
 end
