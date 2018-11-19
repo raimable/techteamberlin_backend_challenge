e477fde2206c38fb2ed0bad0772a377bd926f213
Aimable <raimable@gmail.com>
2018-11-19 08:57:11 +0200

    Apply Rubocop recommendation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
+# Landing page.
 class HomeController < ApplicationController
-
-	def fetch_all
-		if params[:sync]=="true"
-			FetchMissionsJob.perform_now true
-			render json:{success:true, sync:true,message:"All data fetched"} 
-		else
-			FetchMissionsJob.perform_later false
-			render json:{success:true,sync:false,message:"A data fetch is scheduled"}
-		end
-	end 
+  def fetch_all
+    if params[:sync] == 'true'
+      FetchMissionsJob.perform_now true
+      render json: { success: true, sync: true, message: 'All data fetched' }
+    else
+      FetchMissionsJob.perform_later false
+      render json: {
+        success: true, sync: false,
+        message: 'A data fetch is scheduled'
+      }
+    end
+  end
 end
