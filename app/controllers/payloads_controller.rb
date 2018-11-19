4ccea7b72b0c5a9f6161786f6a429fa15a49b418
Aimable <raimable@gmail.com>
2018-11-19 09:04:09 +0200

    Apply Rubocop recommendation

--- app/controllers/payloads_controller.rb
+++ app/controllers/payloads_controller.rb
@@ -1,12 +1,11 @@
+# Payload controller
 class PayloadsController < ApplicationController
-
   def index
     @payloads = Payload.all
     @payloads = @payloads.where(nationality_id: params[:nationality_id]) if params[:nationality_id]
-
   end
 
   def show
-    @payload= Payload.find(params[:id])
+    @payload = Payload.find(params[:id])
   end
 end
