925edf3d98c347c2c35087b9ec2a1e9f9d5d05a3
Aimable <raimable@gmail.com>
2018-11-20 09:10:36 +0200

    Added explicit 404 / Nationality not found handling

--- app/controllers/payloads_controller.rb
+++ app/controllers/payloads_controller.rb
@@ -1,9 +1,17 @@
 # Payload controller
 class PayloadsController < ApplicationController
   def index
-    @payloads = Payload.all
     if params[:nationality_id]
-      @payloads = @payloads.where(nationality_id: params[:nationality_id])
+      nationality = Nationality.find_by_id(params[:nationality_id])
+      if nationality
+        @payloads = @payloads.where(nationality_id: params[:nationality_id])
+      else
+        render json: { success: false, message:
+          'Nationality not found' },
+               status: 404
+      end
+    else
+      @payloads = Payload.all
     end
   end
 
