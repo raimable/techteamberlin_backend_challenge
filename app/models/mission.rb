59fe186af42539efa7ae2bc5c5ca8b93243879bc
Aimable <raimable@gmail.com>
2018-11-20 22:40:18 +0200

    Added validations on mission model

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,6 +3,7 @@ class Mission < ApplicationRecord
   include PgSearch
 
   validates :name, presence: true
+  validates :mission_identifier, uniqueness: true, presence: :true
 
   pg_search_scope :search_by_description, against: :description
   has_many :payloads, dependent: :destroy
