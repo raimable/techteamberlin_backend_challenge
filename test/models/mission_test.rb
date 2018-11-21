9075d0b6aaa86643b8f0591f8ac5ef90acfd5ac1
Aimable <raimable@gmail.com>
2018-11-22 00:08:22 +0200

    Implemented mission model test for CRUD

--- test/models/mission_test.rb
+++ test/models/mission_test.rb
@@ -4,7 +4,35 @@ class MissionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_the_truth
+    mission = Mission.new name: 'some name',
+                          mission_identifier: 'TEST-1-001',
+                          wikipedia: 'missions(:one).wikipedia',
+                          website: 'missions(:one).website',
+                          twitter: 'missions(:one).twitter',
+                          description: 'missions(:one).description'
+    # Test create mission
+    assert mission.save
+    # Test load mission
+    mission_copy = Mission.find(mission.id)
+    # Test if the loaded mission is same as exisiting one
+    assert_equal mission.name, mission_copy.name
+    # Test update mission
+    mission.name = 'Update name'
 
-  mission = Mission.new
-  assert_not mission.save
+    assert mission_copy.save
+    # Test destroy mission
+
+    assert mission.destroy
+  end
+
+  def test_can_not_save_mission_with_no_name
+    mission = Mission.new mission_identifier: 'TEST-2-002',
+                          wikipedia: 'missions(:two).wikipedia',
+                          website: 'missions(:two).website',
+                          twitter: 'missions(:two).twitter',
+                          description: 'missions(:two).description'
+    # Test create mission with no name
+    assert_not mission.save
+  end
 end
